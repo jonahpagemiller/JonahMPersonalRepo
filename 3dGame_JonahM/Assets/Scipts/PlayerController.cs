1eeef8bdead5a6f5ef6fd9ae219ea8a9e7fa7b3e
EvanBrown-o-7 <tcstorrance@gmail.com>
2025-12-19 15:59:21 -0800

    created project

---
+++ 3dGame_JonahM/Assets/Scipts/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+    }
+}
