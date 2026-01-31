52533ef135ef64bd71ee5b0ad426b1f112504713
EvanBrown-o-7 <tcstorrance@gmail.com>
2026-01-31 14:02:15 -0800

    crimor commit

---
+++ Shuck Shot/Assets/Scripts/Playercontroller.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    private Rigidbody rb;
+    private int Timer = 2;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private int jumpForce;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+           transform.position -= transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+          transform.position -= transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+          transform.position += transform.right * speed * Time.deltaTime; ;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(Physics.Raycast(transform.position,Vector3.down,1f))
+            {
+                rb.AddForce(Vector3.up * jumpForce);
+            }
+        }
+
+    }
+
+    private void Shoot()
+    {
+        //need offset from player position
+            Instantiate(bullet, transform.position, Quaternion.identity);
+    }
+}
