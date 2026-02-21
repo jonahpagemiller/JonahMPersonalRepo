b6ca34db1fa052efc69bc3add80361b88bb3bb40
EvanBrown-o-7 <tcstorrance@gmail.com>
2026-02-21 13:04:39 -0800

    made court

--- Shuck Shot/Assets/Scripts/Playercontroller.cs
+++ Shuck Shot/Assets/Scripts/Playercontroller.cs
@@ -20,28 +20,33 @@ public class Playercontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Physics.Raycast(transform.position, Vector3.down, 1.2f))
+            {
+                rb.AddForce(Vector3.up * jumpForce);
+            }
+        }
         if (Input.GetKey(KeyCode.W))
         {
             transform.position += transform.forward * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.A))
         {
-           transform.position -= transform.right * speed * Time.deltaTime;
+            transform.position += -transform.right * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S))
         {
-          transform.position -= transform.forward * speed * Time.deltaTime;
+            transform.position += -transform.forward * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.D))
         {
-          transform.position += transform.right * speed * Time.deltaTime; ;
+            transform.position += transform.right * speed * Time.deltaTime; ;
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetMouseButton(0))
         {
-            if(Physics.Raycast(transform.position,Vector3.down,1f))
-            {
-                rb.AddForce(Vector3.up * jumpForce);
-            }
+            Shoot();
+
         }
 
     }
@@ -49,6 +54,7 @@ public class Playercontroller : MonoBehaviour
     private void Shoot()
     {
         //need offset from player position
-            Instantiate(bullet, transform.position, Quaternion.identity);
+        Instantiate(bullet, transform.position, Quaternion.identity);
+
     }
 }
