dc911c03611d0ad165a18ccb6de9766dca93142c
melong-glide <192152400+TCSTorrance@users.noreply.github.com>
2026-04-18 12:08:48 -0700

    STUFF

--- 3dGame_JonahM/Assets/Scipts/PlayerController.cs
+++ 3dGame_JonahM/Assets/Scipts/PlayerController.cs
@@ -3,6 +3,11 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+    [SerializeField]
+    private float speed;
+    public Camera cam;
+    public Vector3 camOFFset;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,10 +17,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        cam.transform.position = camOFFset + transform.position;
+        cam.transform.LookAt(transform.position);
     }
      void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
+        rb.linearVelocity = new Vector3(movementVector.x,0, movementVector.y)*speed;
+
     }
 }
